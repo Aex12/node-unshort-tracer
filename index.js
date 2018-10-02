1b98012f6013e5ff092c95c6552ca12f786b7d0f
Alex Caston <alexcastonmolero@gmail.com>
2018-10-02 12:51:36 +0200

    First version

---
+++ index.js
@@ -0,0 +1,55 @@
+const parseUrl = require('url').parse
+const http = require('http')
+const https = require('https')
+
+function unshortOneTime (url, method="HEAD") {
+	return new Promise((resolve, reject) => {
+		var parsed_url = parseUrl(url)
+		var protocol = (parsed_url.protocol === "https:") ? https : http
+		var host = parsed_url.host
+		var path = parsed_url.pathname
+
+		var req = protocol.request({method, host, path}, (response) => {
+			resolve(response.headers && response.headers.location || url)
+		})
+
+		req.on('error', (e) => {
+			reject(e)
+		})
+
+		req.end()
+	})
+}
+
+function unshortRecursive (urls, options, resolve, reject, depth=0) {
+	var long_url = urls[urls.length-1]
+
+	if(options.max_depth === depth)
+		return resolve(urls)
+
+	unshortOneTime(long_url, options.method)
+	.then(unshorted_url => {
+		if (unshorted_url === long_url) 
+			resolve(urls)
+		else {
+			urls.push(unshorted_url)
+			unshortRecursive(urls, options, resolve, reject, depth+1)
+		}
+	})
+	.catch(reject)
+}
+
+function unshort (url, options) {
+	options.method = options.method || "HEAD"
+	options.max_depth = options.max_depth || 10
+
+	var urls = []
+	urls.push(url)
+
+	return new Promise((resolve, reject) => {
+		unshortRecursive(urls, options, resolve, reject)
+	})
+}
+
+
+module.exports = unshort
